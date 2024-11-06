8d6fcaa5da8d325740197e94b125285245ea8dbb
Камиль <gafur.kaml@gmail.com>
2024-11-06 17:01:59 +0300

    Проверка

--- Из Файла/Program.cs
+++ Из Файла/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//ggg
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
